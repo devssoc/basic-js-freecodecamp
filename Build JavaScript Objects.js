14fbfa8c8668903f82449e3e0e7e04519f53e33a
devssoc <souranshuchatterjee0220@gmail.com>
2022-09-13 21:38:46 +0530

    js

---
+++ Build JavaScript Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+  "name": "Kausik",
+  "legs": 5,
+  "tails": 1,
+  "friends": ["Ashirvad" ,"Bhavuk"]
+  
+    // Only change code above this line
+  };
